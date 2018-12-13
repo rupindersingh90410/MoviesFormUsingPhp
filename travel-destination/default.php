4ba35dfe2dd2835d77ffc8dddd3259a6abf2e8ea
rupindersingh90410 <43187871+rupindersingh90410@users.noreply.github.com>
2018-12-13 13:52:17 -0500

    Add files via upload

--- travel-destination/default.php
+++ travel-destination/default.php
@@ -1,4 +1,14 @@
-<?php $page_title = 'Default';
-require_once('header.php'); ?>
+<?php
+$title = "Destination";
+require('header.php');
+?>
 
-<?php require_once('footer.php'); ?>
+<main class="well text-center">
+    <h1 class="">Travel Bug</h1>
+    <p>This site was created for COMP1006 By Rupinder Singh.It uses PHP, MySQL, and Twitter Bootstrap and is deployed to AWS.</p>
+    <p>Source Code is available at <a href="https://github.com/rupindersingh90410/TravelBugsFormUsingPhp/tree/master/FinalProject">
+            https://github.com/rupindersingh90410/TravelBugsFormUsingPhp/tree/master/Finalproject</a>.</p>
+</main>
+
+</body>
+</html>
